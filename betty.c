7ca4c370ba62268e01aba0c8f36cacebdd1304a0
JoshAwuro <joshuawuro@gmail.com>
2023-05-19 20:34:44 -0700

    Proud

---
+++ betty.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int add_numbers(int a, int b)
+{
+	int sum a + b;
+	
+	return sum;
+}
+
+int main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	int result;
+
+	result = add_numbers(num1, num2);
+	printf("The sm of %d and %d is %d\n", num1, num2, results);
+
+	return 0;
+}
