130e33afa2d2ef3cb9b5af7b1730fc21684a787e
JoshAwuro <joshuawuro@gmail.com>
2023-05-24 19:42:06 -0700

    simple1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point to program
+ * @ac: No of cmd-line arguments
+ * @av: Array of cmd-line arguments
+ * @env: Array of environ variables
+ *
+ * Return: Always 0
+ */
+int main(int ac, char **av, char **env)
+{
+	if (ac == 1)
+		prompt(av, env);
+	return (0);
+}
