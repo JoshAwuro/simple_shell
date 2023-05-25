d8b3743aeae7d6ca28bde522c4b2f1b6752cb404
JoshAwuro <joshuawuro@gmail.com>
2023-05-24 19:09:15 -0700

    HEADER

--- shell.h
+++ shell.h
@@ -1,35 +1,55 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef SHELL_H
+#define SHELL_H
+/*---MODE---*/
+void interactive_mode(void);
+void non_interactive_mode(void);
 
+/*---MACROS---*/
+extern char **environ;
+#define MAX_NUM 10
+#define MAX_LENGHT 256
+#define BUFFER_SIZE 1024
+
+/*---LIBRARIES---*/
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <dirent.h>
-#include <errno.h>
-#include <sys/wait.h>
+#include <string.h>
 #include <sys/types.h>
-#include <ctype.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 #include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
-extern char **environ;
-char *_strcat(char *end, char *src);
-void prompt(void);
-char *_strcat(char *end, char *src);
-char prints_inputs(void);
-int _strlen(char *s);
-void hold(char *h);
-char *command_finder(char *cmd);
-char *file_finder(char *cmd);
-int compare(char *str1, char *str2);
-int _strcmp(char *str1, char *str2);
-char *concat_str(char *str1, char *str2);
-int _putchar(char ch);
-int slash(char *command);
-int check_exit(char *str1, char *str2);
-int env_check(char *str1, char *str2);
-char **strput(char *arg);
-void controlled(int signal);
-void perform(char **command);
+/*---PROTOTYPES---*/
+char *read_stdin(void);
+char *read_stream(void);
+char **tokeniz(char *line);
+int excutcmd(char **cmd);
+void errors(char *s);
+char *_strdup(char *s);
+int _strlen(const char *s);
+int _strncmp(const char *str1, const char *str2, int n);
+char *_getenv(const char *name);
+void error(int status, char **s, int running);
+void _itoa(int num, char *str);
+char *_strcat(char *dest, char *src);
+int end(char **cmd);
+char *_strcpy(char *dest, char *src);
+int env(char **cmd);
+char *_strtok(char *str, char *delim);
+
+/*---STRUCTRES---*/
+/**
+ * struct built - structure for builtins.
+ * @name: name of the builtin.
+ * @f: function prototype.
+ */
+typedef struct built
+{
+	char *name;
+	int (*f)(char **);
+} built_in;
 
 #endif
