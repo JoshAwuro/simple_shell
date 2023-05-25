2283c9d5f7861991d6668616315fe75ad3ddf728
JoshAwuro <joshuawuro@gmail.com>
2023-05-24 19:32:29 -0700

    initiate

---
+++ main.h
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MAX_COMMAND 10
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stddef.h>
+
+extern char **environ;
+int space_check(char *lineptr);
+char *_getenv(const char *name);
+size_t _strcspn(const char *str, const char *rej);
+char *_strdup(const char *str);
+char *_strchr(const char *s, int c);
+int _strcmp(const char *s1, const char *s2);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+size_t _strlen(const char *str);
+int _strncmp(const char *str1, const char *str2, size_t n);
+int _puts(const char *str);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void prompt(char **av, char **env);
+char *build_cmd_path(char *dir, char *command);
+void handle_cmd(char **argv, char **env, char **path_parts, int path_index);
+void print_environment(char **env);
+char *find_cmd_path(char *command, char **path_parts, int path_index);
+void exec_cmd(char *cmd_path, char **argv, char **env);
+void init_path(int *path_index, char *path_parts[]);
+
+#endif
